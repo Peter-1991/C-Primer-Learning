0e464e022b24109356215dbf009dc59efc2dca68
Peter-1991 <496259634@qq.com>
2015-11-15 18:06:10 +0800

    first commit

---
+++ C++Primer/C++Primer/main.cpp
@@ -0,0 +1,170 @@
+//
+//  main.cpp
+//  C++Primer
+//
+//  Created by Dracarys on 15/11/8.
+//  Copyright (c) 2015年 Dracarys. All rights reserved.
+//
+
+#include <iostream>
+#include "Sales_item.h"
+
+std::string global_string;
+int global_int = 42;
+
+class Foo {
+    
+};
+
+int main(int argc, const char * argv[]) {
+//    std::cout << "小杰\n";
+
+//    std::cout << "Enter two number:" <<std::endl;
+//    int v1, v2;
+//    std::cin >> v1;
+//    std::cin >> v2;
+//    std::cout << "The sum of " << v1 ;
+//    std::cout << " and " << v2;
+//    std::cout <<" is " << v1 * v2;
+//    std::cout << std::endl;
+
+    
+//    std::cout << "/*" << std::endl;
+//    std::cout << "*/" << std::endl;
+//    std::cout << /* "*/" */" << std::endl;
+    
+//    int sum = 0;
+//    for (int i = -100; i <= 100; ++i) {
+//        sum += i;
+//    }
+//    std::cout << sum << std::endl;
+
+    
+//    for (int i = 50 ; i <= 100; ++i) {
+//        sum += i;
+//    }
+//    
+//    std::cout << sum << std::endl;
+//    
+//    sum = 0;
+//    int i = 50;
+//    while (i <= 100) {
+//        sum += i;
+//        i += 1;
+//    }
+//    
+//    std::cout << sum << std::endl;
+    
+//    int num = 10;
+//    while (num >= 0) {
+//        std::cout << num << std::endl;
+//        num -= 1;
+//    }
+//    
+//    for (int i = 10; i >= 0; --i) {
+//        std::cout << i << std::endl;
+//        
+//    }
+    
+//    int countNum, value;
+//    while (std::cin >> value) {
+//        if (value < 0) {
+//            countNum++;
+//        }
+//    }
+//    
+//    std::cout << countNum;
+    
+//    Sales_item book;
+//    
+//    while (std::cin >> book) {
+//        std::cout << book << std::endl;
+//    }
+    
+//    Sales_item book1, book2;
+//    
+//    std::cin >> book1 >> book2;
+//    
+//    if (book1.same_isbn(book2)) {
+//        std::cout << book1 + book2;
+//    }
+    
+    
+//    Sales_item total, book;
+//    
+//    if (std::cin >> total) {
+//        while (std::cin >> book) {
+//            if (total.same_isbn(book)) {
+//                total = total + book;
+//            }else{
+//                std::cout << "Different ISBN Book" << std::endl;
+//                return -1;
+//            }
+//            std::cout << total << std::endl;
+//        }
+//    }else{
+//        std::cout << "No Data?!" << std::endl;
+//        return -1;
+//    }
+
+    
+//    Sales_item total, trans;
+//    
+//    if (std::cin >> total) {
+//        while (std::cin >> trans) {
+//            if (total.same_isbn(trans)) {
+//                total = total + trans;
+//            }else{
+//                total = trans;
+//            }
+//        }
+//        std::cout << total << std::endl;
+//    }else{
+//        std::cout << "No Data?!" << std::endl;
+//        return -1;
+//    }
+    
+//    int base, exponent;
+//    
+//    long result = 1;
+//    
+//    std::cout << "Enter base and exponent" << std::endl;
+//    std::cin >> base >> exponent;
+//    
+//    if (exponent < 0) {
+//        std::cout << "Exponent can't be smaller than 0" << std::endl;
+//        return -1;
+//    }
+//    
+//    if (exponent > 0) {
+//        for (int cnt = 1; cnt <= exponent; ++cnt) {
+//            result *= base;
+//        }
+//    }
+//    
+//    std::cout << "base:" << base << " exponent:" << exponent << " result:" << result;
+
+    
+    
+//    int local_int;
+//    std::string local_string;
+//    
+//    int global_int = 100;
+//    int j = global_int;
+//    std::cout << j << std::endl;
+//    
+
+//    int i = 100, sum = 0;
+//    
+//    for (int i = 0; i != 10; ++i) {
+//        sum += i;
+//    }
+//    std::cout << i << std::endl;
+//    std::cout << sum << std::endl;
+    
+    
+
+    
+    
+    return 0;
+}
